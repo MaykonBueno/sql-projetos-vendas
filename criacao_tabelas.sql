9f92f067de076b28a48e0ccffdcb55559191ce75
MaykonBueno <39282653+MaykonBueno@users.noreply.github.com>
2026-03-12 22:58:02 -0300

    Update criacao_tabelas.sql

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -1,33 +1,40 @@
 CREATE TABLE clientes (
-id_cliente INT PRIMARY KEY,
+id_cliente INT AUTO_INCREMENT PRIMARY KEY, 
 nome VARCHAR(100),
-email VARCHAR(100)
+email VARCHAR(100),
+telefone VARCHAR(20),
+data_cadastro DATETIME  
 );
 
 CREATE TABLE produtos (
-id_produto INT PRIMARY KEY,
+id_produto INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(100),
+descricao VARCHAR(100),
 preco DECIMAL(10,2),
-estoque INT
+estoque INT,
+data_cadastro DATETIME   
 );
 
 CREATE TABLE vendedores (
-id_vendedor INT PRIMARY KEY,
-nome VARCHAR(100)
+id_vendedor INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100),
+email VARCHAR(100),
+comissao DECIMAL (5,2)
 );
 
 CREATE TABLE vendas (
-id_venda INT PRIMARY KEY,
+id_venda INT AUTO_INCREMENT PRIMARY KEY,
 id_cliente INT,
 id_vendedor INT,
 data_venda DATE,
+valor_total INT,
 
 FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
 FOREIGN KEY (id_vendedor) REFERENCES vendedores(id_vendedor)
 );
 
 CREATE TABLE itens_venda (
-id_item INT PRIMARY KEY,
+id_item INT AUTO_INCREMENT PRIMARY KEY,
 id_venda INT,
 id_produto INT,
 quantidade INT,
