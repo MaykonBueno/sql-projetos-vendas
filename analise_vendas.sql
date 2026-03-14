92727c322566d23e7cc89545df0b5917891ea1b0
MaykonBueno <39282653+MaykonBueno@users.noreply.github.com>
2026-03-13 23:07:07 -0300

    Create analise_vendas.sql

---
+++ analise_vendas.sql
@@ -0,0 +1,35 @@
+-- faturamento total
+SELECT 
+MONTH (v.data_venda) AS  mes,
+SUM(iv.quantidade * iv.preco_unitario) as faturamento_total
+FROM vendas v
+JOIN itens_venda iv ON v.id_venda = iv.id_venda
+GROUP BY MONTH (v.data_venda)
+ORDER BY mes;
+
+-- produto mais vendido
+SELECT 
+p.nome,
+SUM(iv.quantidade) AS mais_vendido
+FROM produtos p
+JOIN itens_venda iv ON p.id_produto = iv.id_produto
+GROUP BY p.id_produto, p.nome
+ORDER BY mais_vendido DESC
+LIMIT 1;
+
+-- top clientes
+SELECT
+c.nome,
+SUM(iv.quantidade * iv.preco_unitario) AS valor_total_gasto
+FROM cliente c
+JOIN vendas v ON c.id_cliente = v.id_cliente
+JOIN  itens_venda iv ON v.id_venda = iv.id_venda 
+GROUP BY c.id_cliente, c.nome
+ORDER BY valor_total_gasto DESC
+LIMIT 3;
+
+-- ticket médio
+SELECT 
+SUM(iv.quantidade*iv.preco_unitario) / COUNT(DISTINCT v.id_venda) as ticket_medio
+FROM vendas v
+JOIN itens_vendas iv ON v.id_venda = iv.id_venda;
